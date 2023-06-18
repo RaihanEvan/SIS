466c0089780d35393f687b68a48ea214b5f7b37c
Raihan Evan <raihan.evan@gmail.com>
2023-06-18 12:51:50 +0600

    created student class

--- src/application/Student.java
+++ src/application/Student.java
@@ -1,5 +1,51 @@
 package application;
 
 public class Student {
+	private Integer id;
+	private String name;
+	private String email;
+	private String contact;
+	
+	public Student(Integer id, String name, String email, String contact) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.contact = contact;
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	
+	
 }
