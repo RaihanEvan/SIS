ae16f9bde4787a62f76824849c91abdce04e1f4a
Raihan Evan <raihan.evan@gmail.com>
2023-06-18 15:53:13 +0600

    controller added

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,11 +12,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
+			BorderPane root2 = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("SceneFXML.fxml"));
+			Scene scene = new Scene(root,920,600);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Student Information System");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/application/SceneFXMLController.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SceneFXMLController implements Initializable{
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+}
