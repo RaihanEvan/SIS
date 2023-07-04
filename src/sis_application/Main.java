49a6a739129ef03d4f4039b9ad723a860f745781
Raihan Evan <raihan.evan@gmail.com>
2023-07-04 14:46:06 +0600

    removed unused codes

--- src/sis_application/Main.java
+++ src/sis_application/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("SceneFXML.fxml"));
-			BorderPane root2 = new BorderPane();
+			//BorderPane root2 = new BorderPane();
 			Scene scene = new Scene(root,920,600);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/sis_application/OwnerInfoController.java
+++ src/sis_application/OwnerInfoController.java
@@ -1,13 +1,5 @@
 package sis_application;
 
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class OwnerInfoController{
 
