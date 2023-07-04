2f5d7aa16dd4ed1fb906c6b37bcba3befa8453f8
Raihan Evan <raihan.evan@gmail.com>
2023-07-04 13:35:30 +0600

    Added a separate class following SOLID
    In this version the db operations are encapsulated within the StudentDAO class with a more cleaner separation of functions also following SOLID principles. The StudentDAO class handles all db-related tasks, SceneFXMLController class focuses on managing the UI and invoking appropriate methods from the StudentDAO class.

--- src/sis_application/SceneFXMLController.java
+++ src/sis_application/SceneFXMLController.java
@@ -1,28 +1,19 @@
 package sis_application;
 
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-public class SceneFXMLController implements Initializable{
-	@FXML
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SceneFXMLController implements Initializable {
+    @FXML
     private Button btnDelete;
 
     @FXML
@@ -57,155 +48,96 @@ public class SceneFXMLController implements Initializable{
 
     @FXML
     private TextField tfPhone;
-    
+
     @FXML
     private Label lblStatus;
-    
+
+    private StudentDAO studentDAO;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        studentDAO = new StudentDAO();
+        showStudent();
+    }
+
     @FXML
     void handleButtonAction(ActionEvent event) {
-		System.out.println("Button is clicked");
-		try {
-//			Label label = new Label();
-//			label.setText("Hello Java");
-			if(event.getSource() == btnInsert) {
-				insertRecord();
-				System.out.println("Data Inserted Successfully");
-				lblStatus.setText("Data Inserted Successfully");
-			}else if(event.getSource() == btnUpdate) {
-				updateRecord();
-				System.out.println("Data Updated Successfully");
-				lblStatus.setText("Data Updated Successfully");
-			}else if(event.getSource() == btnDelete) {
-				deleteRecord();
-				System.out.println("Data Deleted Successfully");
-				lblStatus.setText("Data Deleted Successfully");
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}	
+        if (event.getSource() == btnInsert) {
+            insertRecord();
+        } else if (event.getSource() == btnUpdate) {
+            updateRecord();
+        } else if (event.getSource() == btnDelete) {
+            deleteRecord();
+        }
     }
-	
-   
-    public void initialize(URL url, ResourceBundle rb) {
-    	showStudent();
-    }
-    
-    public Connection getConnection() throws SQLException {
-    	Connection conn = null;
-    	try {
-    		Class.forName("com.mysql.cj.jdbc.Driver"); 
-    		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
-    		System.out.println("DB Connected");
-    	}catch(Exception e) {
-    		System.out.println("Error: "+e.getMessage());
-//    		return null;
-    	}
-    	return conn;
-    }
-    	public ObservableList<Student> getStudentList(){
-    		ObservableList<Student> studentList = FXCollections.observableArrayList();
-    		Connection conn = null;
-			try {
-				conn = getConnection();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-    		String query = "SELECT * FROM student";
-    		Statement st;
-    		ResultSet rs;
-    		try {
-    			st= conn.createStatement();
-    			rs = st.executeQuery(query);
-    			Student student;
-    			while(rs.next()) {
-    				student = new Student(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"));
-    				studentList.add(student);
-    			}
-    		}catch(Exception e) {
-    			e.printStackTrace();
-    		}
-    		return studentList;
-    	}
-    	public void showStudent() {
-    		ObservableList<Student> list = getStudentList();
-    		colId.setCellValueFactory(new PropertyValueFactory<Student,Integer>("id"));
-    		colName.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
-    		colEmail.setCellValueFactory(new PropertyValueFactory<Student,String>("email"));
-    		colPhone.setCellValueFactory(new PropertyValueFactory<Student,String>("phone"));
-    		
-    		tbStudent.setItems(list);
-    	}
-    
-    	private void insertRecord(){
-    		try {
-    			Connection conn = getConnection();
-    			Statement stmt = conn.createStatement();
-    	        int id = Integer.parseInt(tfId.getText());
-    	        String name = tfName.getText();
-    	        String email = tfEmail.getText();
-    	        String phone = tfPhone.getText();
-    	        String query = "INSERT INTO student VALUES (" + id + ",'" + name + "','" + email + "','" + phone + "')";
-    	        stmt.executeUpdate(query);
-    		}catch (SQLException e) {
-                e.printStackTrace();
-            }
-            showStudent();
-    	}
-    
-    	private void updateRecord() {
-            try {
-            	Connection conn = getConnection();
-                Statement stmt = conn.createStatement();
-                int id = Integer.parseInt(tfId.getText());
-                String name = tfName.getText();
-                String email = tfEmail.getText();
-                String phone = tfPhone.getText();
 
-                String query = "UPDATE student SET name = '" + name + "', email = '" + email + "', phone = '" + phone + "' WHERE id = " + id;
-                stmt.executeUpdate(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    private void insertRecord() {
+        int id = Integer.parseInt(tfId.getText());
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        Student student = new Student(id, name, email, phone);
+        boolean success = studentDAO.insertStudent(student);
+        if (success) {
             showStudent();
+            lblStatus.setText("Data Inserted Successfully");
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Insert Error", "Failed to insert data");
         }
-    	
-    	private void deleteRecord() {
-            try{
-            	Connection conn = getConnection();
-                Statement stmt = conn.createStatement();
-                int id = Integer.parseInt(tfId.getText());
-                String query = "DELETE FROM student WHERE id = " + id;
-                stmt.executeUpdate(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            showStudent();
-        }
-    	
-    	private void executeQuery(String query) {
-    		Connection conn = null;
-			try {
-				conn = getConnection();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-    		Statement st;
-    		try {
-    			st = conn.createStatement();
-    			st.executeUpdate(query);
-    		}catch(Exception e) {
-    			e.printStackTrace();
-    		}
-    	}
-    	
-        @FXML
-        private void handleMouseAction(MouseEvent event) {
-        	Student student = tbStudent.getSelectionModel().getSelectedItem();
-        	tfId.setText(""+ student.getId());
-        	tfName.setText(student.getName());
-        	tfEmail.setText(""+ student.getEmail());
-        	tfPhone.setText(""+ student.getPhone());
+    }
 
+    private void updateRecord() {
+        int id = Integer.parseInt(tfId.getText());
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        Student student = new Student(id, name, email, phone);
+        boolean success = studentDAO.updateStudent(student);
+        if (success) {
+            showStudent();
+            lblStatus.setText("Data Updated Successfully");
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Update Error", "No record found with ID: " + id);
         }
+    }
+
+    private void deleteRecord() {
+        int id = Integer.parseInt(tfId.getText());
+        boolean success = studentDAO.deleteStudent(id);
+        if (success) {
+            showStudent();
+            lblStatus.setText("Data Deleted Successfully");
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Delete Error", "No record found with ID: " + id);
+        }
+    }
+
+    private void showStudent() {
+        ObservableList<Student> list = FXCollections.observableArrayList(studentDAO.getAllStudents());
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+
+        tbStudent.setItems(list);
+    }
+
+    @FXML
+    private void handleMouseAction(MouseEvent event) {
+        Student student = tbStudent.getSelectionModel().getSelectedItem();
+        if (student != null) {
+            tfId.setText(String.valueOf(student.getId()));
+            tfName.setText(student.getName());
+            tfEmail.setText(student.getEmail());
+            tfPhone.setText(student.getPhone());
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
-	
---
+++ src/sis_application/StudentDAO.java
@@ -0,0 +1,83 @@
+package sis_application;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAO {
+    private Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "");
+            System.out.println("DB Connected");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> studentList = new ArrayList<>();
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM student")) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                Student student = new Student(id, name, email, phone);
+                studentList.add(student);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return studentList;
+    }
+
+    public boolean insertStudent(Student student) {
+        String query = "INSERT INTO student (id, name, email, phone) VALUES (?, ?, ?, ?)";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, student.getId());
+            stmt.setString(2, student.getName());
+            stmt.setString(3, student.getEmail());
+            stmt.setString(4, student.getPhone());
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateStudent(Student student) {
+        String query = "UPDATE student SET name = ?, email = ?, phone = ? WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, student.getName());
+            stmt.setString(2, student.getEmail());
+            stmt.setString(3, student.getPhone());
+            stmt.setInt(4, student.getId());
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteStudent(int id) {
+        String query = "DELETE FROM student WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
