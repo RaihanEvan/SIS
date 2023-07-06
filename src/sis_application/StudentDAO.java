62e4583c69c2916cb528ff17bd9f01b568eea2fc
Raihan Evan <raihan.evan@gmail.com>
2023-07-06 13:36:46 +0600

    added comments and proper documentation

--- src/sis_application/StudentDAO.java
+++ src/sis_application/StudentDAO.java
@@ -12,6 +12,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class StudentDAO {
+	
+	//DB CONNECTION
+	
     private Connection getConnection() throws SQLException {
         Connection conn = null;
         try {
@@ -24,18 +27,23 @@ public class StudentDAO {
         return conn;
     }
 
+    //READ OPERATION
+    
     public List<Student> getAllStudents() {
     	ObservableList<Student> studentList = FXCollections.observableArrayList();
 //        List<Student> studentList = new ArrayList<>();
-        try (Connection conn = getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM student")) {
+        try{
+        	Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM student");
+            
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String email = rs.getString("email");
                 String phone = rs.getString("phone");
                 Student student = new Student(id, name, email, phone);
+                
                 studentList.add(student);
             }
         } catch (SQLException e) {
@@ -44,14 +52,18 @@ public class StudentDAO {
         return studentList;
     }
 
+    //CREATE OPERATION
+    
     public boolean insertStudent(Student student) {
         String query = "INSERT INTO student (id, name, email, phone) VALUES (?, ?, ?, ?)";
-        try (Connection conn = getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+        try{
+        	Connection conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setInt(1, student.getId());
             stmt.setString(2, student.getName());
             stmt.setString(3, student.getEmail());
             stmt.setString(4, student.getPhone());
+            
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
@@ -60,14 +72,18 @@ public class StudentDAO {
         return false;
     }
 
+    //UPDATE OPERATION
+    
     public boolean updateStudent(Student student) {
         String query = "UPDATE student SET name = ?, email = ?, phone = ? WHERE id = ?";
-        try (Connection conn = getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+        try{
+        	Connection conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, student.getName());
             stmt.setString(2, student.getEmail());
             stmt.setString(3, student.getPhone());
             stmt.setInt(4, student.getId());
+            
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
@@ -75,11 +91,14 @@ public class StudentDAO {
         }
         return false;
     }
-
+    
+    //DELETE OPERATION
+    
     public boolean deleteStudent(int id) {
         String query = "DELETE FROM student WHERE id = ?";
-        try (Connection conn = getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+        try{
+        	Connection conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setInt(1, id);
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
@@ -88,6 +107,8 @@ public class StudentDAO {
         }
         return false;
     }
+    
+    //Duplicate student check
     
     public boolean checkStudentExists(int studentId) {
         try (Connection conn = getConnection();
